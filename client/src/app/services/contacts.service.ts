6497bc45a92f505a543f9bf98575ecc425d0a2bc
Ankit Gupta <gupta.ankit72@gmail.com>
2018-05-07 23:54:52 +0530

    clean UI architecture for building the app

---
+++ client/src/app/services/contacts.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import {Http , Headers} from "@angular/http";
+import {Contact} from "../contact";
+@Injectable()
+export class ContactsService {
+
+  constructor() { }
+
+}
